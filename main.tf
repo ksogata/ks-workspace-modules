c03f4d3b2634c16c1e85c62385681ffdfbe2e35d
Kevin Sogata <ksogata@ksmac.local>
2021-01-28 13:28:34 -0800

    update module

--- main.tf
+++ main.tf
@@ -1,26 +1,35 @@
-variable "name" {}
-variable "location" {}
-variable "username" {}
-variable "password" {}
-
-provider "azurerm" {
-  features {}
-}
-
-variable "vnet_address_spacing" {
-  type = "list"
-}
-
-variable "subnet_address_prefixes" {
-  type = "list"
-}
-
-module "networking" {
-  source  = "app.terraform.io/kevins-org/networking/azurerm"
+module "webserver" {
+  source  = "app.terraform.io/YOUR_ORG_NAME/webserver/azurerm"
   version = "0.12.0"
 
-  name                    = var.name
-  location                = var.location
-  vnet_address_spacing    = var.vnet_address_spacing
-  subnet_address_prefixes = var.subnet_address_prefixes
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[0]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "appserver" {
+  source  = "app.terraform.io/YOUR_ORG_NAME/appserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[1]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
+}
+
+module "dataserver" {
+  source  = "app.terraform.io/YOUR_ORG_NAME/dataserver/azurerm"
+  version = "0.12.0"
+
+  name      = var.name
+  location  = var.location
+  subnet_id = module.networking.subnet-ids[2]
+  vm_count  = 1
+  username  = var.username
+  password  = var.password
 }
