3c83229ee2d8e87f6baf2f37c4a87d06d9cf3dd8
Kevin Sogata <ksogata@ksmac.local>
2021-01-28 13:29:47 -0800

    accidentally deleted other modules

--- main.tf
+++ main.tf
@@ -1,3 +1,30 @@
+variable "name" {}
+variable "location" {}
+variable "username" {}
+variable "password" {}
+
+provider "azurerm" {
+  features {}
+}
+
+variable "vnet_address_spacing" {
+  type = "list"
+}
+
+variable "subnet_address_prefixes" {
+  type = "list"
+}
+
+module "networking" {
+  source  = "app.terraform.io/kevins-org/networking/azurerm"
+  version = "0.12.0"
+
+  name                    = var.name
+  location                = var.location
+  vnet_address_spacing    = var.vnet_address_spacing
+  subnet_address_prefixes = var.subnet_address_prefixes
+}
+
 module "webserver" {
   source  = "app.terraform.io/YOUR_ORG_NAME/webserver/azurerm"
   version = "0.12.0"
